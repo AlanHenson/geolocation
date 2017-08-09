852213d3586a92a404aa940c17def7c422c1b0b0
Alan Henson <alan.henson@campeagle.camp>
2017-08-09 17:33:02 -0600

    debugging accounts not showing up

--- force-app/main/default/aura/AccountList/AccountListController.js
+++ force-app/main/default/aura/AccountList/AccountListController.js
@@ -2,7 +2,8 @@
     doInit : function(component, event) {
         var action = component.get("c.findAll");
         action.setCallback(this, function(a) {
-          component.set("v.accounts", a.getReturnValue()); 
+          component.set("v.accounts", a.getReturnValue());
+          console.log(a.getReturnValue());
         });
         $A.enqueueAction(action);
     }
